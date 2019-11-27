0250989bd2d9f8cde54b203117c692713c56e3be
Yerukumajji <devendra.yerukumajji@atos.net>
2019-11-27 17:17:37 +0530

    first commit to push inoto github

---
+++ app/config.php
@@ -0,0 +1,12 @@
+<?php
+
+	return [
+		's3' => [
+			'key' => 'AKIARMTAE6QXM5ZRGMJT',
+			'secret' => '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',
+			'bucket' => 'test-dev.website.com'
+		]
+
+	];
+
+?>
